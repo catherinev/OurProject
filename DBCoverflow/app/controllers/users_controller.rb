18d67defeadc4d8117bf4fab0b0c0659f7ba25b8
Catherine V & Monica Cho <pair+catherinev.monicadev@devbootcamp.com>
2014-06-25 15:15:42 -0500

    can create new user account and route if failed

--- DBCoverflow/app/controllers/users_controller.rb
+++ DBCoverflow/app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 
 class UsersController < ApplicationController
 	
-	def new
-
-	end
-	
 	def create
-
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to questions_path
+		else
+			@error = "Invalid shit.  Try again."
+			render new_user_path
+		end
 	end
 
 	def edit
@@ -19,6 +22,11 @@ class UsersController < ApplicationController
 
 	def update
 	
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:username, :email, :password, :password_confirmation)
 	end
 
 end
