ce5307935569cd3ecefe0e8cebd4588dcc79a09f
catherinev <catherinev@users.noreply.github.com>
2014-06-25 22:21:28 -0500

    custom routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,36 @@
 Rails.application.routes.draw do
 
   resources :users, except: [:destroy, :index]
-  resources :questions do
-    resources :comments
-    resources :answers
-  end
+
+  get '/questions/:question_id/comments', to: 'comments#index', as: :question_comments 
+  post '/questions/:question_id/comments', to:     "comments#create"
+  get '/questions/:question_id/comments/new', to: "comments#new", as: :new_question_comment
+  get '/questions/:question_id/comments/:id/edit',to: "comments#edit", as: :edit_question_comment
+  get '/questions/:question_id/comments/:id', to: "comments#show", as: :question_comment
+  patch  '/questions/:question_id/comments/:id', to: "comments#update"
+  put    '/questions/:question_id/comments/:id', to: "comments#update"
+  delete '/questions/:question_id/comments/:id', to: "comments#destroy"
+  get '/questions/:question_id/answers', to:     "answers#index", as: :question_answers
+  post   '/questions/:question_id/answers', to: "answers#create"
+  get '/questions/:question_id/answers/new', to:  "answers#new", as: :new_question_answer
+  get '/questions/:question_id/answers/:id/edit', to: "answers#edit", as: :edit_question_answer
+  get '/questions/:question_id/answers/:id', to:  "answers#show", as: :question_answer
+  patch  '/questions/:question_id/answers/:id', to:  "answers#update"
+  put    '/questions/:question_id/answers/:id', to:  "answers#update"
+  delete '/questions/:question_id/answers/:id', to:  "answers#destroy"
+  get '/questions', to: 'questions#index', as: :questions
+  post '/questions', to: 'questions#create'
+  get '/questions/new', to: 'questions#new', as: :new_question
+  get '/questions/:id/edit', to: 'questions#edit', as: :edit_question
+  get '/questions/:id', to: 'questions#show', as: :question
+  patch '/questions/:id', to: 'questions#update'
+  put '/questions/:id', to: 'questions#update'
+  delete '/questions/:id', to: 'questions#destroy'
+
+  # resources :questions do
+  #   resources :comments
+  #   resources :answers
+  # end
   resources :answers, only: [] do
     resources :comments
   end
