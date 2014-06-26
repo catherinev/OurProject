c3e85b2d728a88269a82400b7a9b307c44f06e19
Catherine V & Monica Cho <pair+catherinev.monicadev@devbootcamp.com>
2014-06-26 14:44:27 -0500

    added answer model validations and nothing broke

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,5 +2,7 @@ class Answer < ActiveRecord::Base
   has_many :comments, as: :parent
   belongs_to :user
 
-
+  validates :question_id, presence: true
+  validates :content, presence: true
+  validates :user_id, presence: true
 end
