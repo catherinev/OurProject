d2a2eb4c15d71975a6c0dcb24a47f5300b21876b
Kim Girard <kimberley.girard@gmail.com>
2014-06-27 13:35:18 -0500

    create test for login button

--- spec/features/login_and_out_spec.rb
+++ spec/features/login_and_out_spec.rb
@@ -1,1 +1,11 @@
+require 'spec_helper'
 
+feature 'User logging in' do
+	context 'on questions#show' do
+		it "sees a Login link" do
+			visit root_url
+			expect(page).to have_content("Login")
+		end
+	end
+
+end
