50e56ab1224a8e2141a70353d3bba2cbc94a1c56
Kim Girard <kimberley.girard@gmail.com>
2014-06-27 15:23:01 -0500

    add form class to sessions_new and login error tests

--- spec/features/login_and_out_spec.rb
+++ spec/features/login_and_out_spec.rb
@@ -30,6 +30,18 @@ feature 'User logging in' do
 			expect(current_url).to eq questions_url
 			expect{session[:user_id]}.to_not be nil
 		end
+
+		it "should not log in an unregistered user" do	
+			visit new_session_url
+			within('.loginform') do
+				fill_in 'Email', with: "abcdefghi@jklmnop.com"
+				fill_in 'Password', with: "password"
+			end
+			click_button('Sign In')
+			expect(page).to have_content("Your email and password do not match")
+			expect(current_url).to eq sessions_url
+
+		end
 	end
 
 end
