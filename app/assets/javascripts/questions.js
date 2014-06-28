f35bd7bc682a5b9b7b78272f8607446981c89c47
Catherine V & Monica Cho <pair+catherinev.monicadev@devbootcamp.com>
2014-06-28 12:46:10 -0500

    fixed small question comment bug

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -35,16 +35,21 @@ $(document).ready( function(){
   });
 
   });
+
+
   $('.commentform').on('submit', function(event){
     event.preventDefault();
+
     var data = $(this).serialize();
-    var question_id = $(this).attr('id').substr(1);
+    var question_id = $(this).attr('id');
     var url =  '/questions/' + question_id +'/comments';
+
     $.post(url, data, function(response){
       $('.commentform')[0].reset();
       $('.commentform').hide();
       $('.addComment').show();
       $('.comments').prepend(response);
+     
 
     }).fail(function(response) {
     console.log(response)
